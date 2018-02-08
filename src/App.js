87d2bc97d87663693743ef56c9980199aef7b2f4
Darren Broderick <darren.broderick@liberty-it.co.uk>
2018-02-08 11:28:08 +0000

    Added es lint

--- src/App.js
+++ src/App.js
@@ -1,57 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
+import FontAwesome from 'react-fontawesome';
 import './App.css';
 
 class App extends Component {
-    render() {
+  render() {
+    const options = ['Invictus', 'Telsa', 'Sigma', 'Polaris'];
+    // const defaultOption = options[0];
 
-        const options = ['Invictus', 'Telsa', 'Sigma', 'Polaris'];
-        const defaultOption = options[0];
-
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to the LMB Metrics Dashboard</h1>
-                </header>
-                <p className="App-intro">
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to the LMB Metrics Dashboard</h1>
+        </header>
+        <p className="App-intro">
                     Please select a team below for further information.
-                </p>
+        </p>
 
-                <p><Dropdown options={options} onChange={this._onSelect} value="LMB Teams" /></p>
+        <span>
+          <Dropdown options={options} value="LMB Teams" />
+          <FontAwesome
+            className="super-crazy-colors"
+            name="caret-down"
+            size="lg"
+            style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }} />
+        </span>
 
-                <div className="flex-container">
-                    <div>Code Coverage
-                        <span className="small-text">98%</span>
-                    </div>
-                    <div>Security
-                        <span className="small-text">No Security Issues</span>
-                    </div>
-                    <div>Bugs
-                        <span className="small-text">7 Critical Items</span>
-                    </div>
-                    <div>New Relic
-                        <span className="small-text">0.114% Error Rate</span>
-                    </div>
-                    <div>AWS Instances
-                        <span className="small-text">67 EC2 Instances</span>
-                    </div>
-                    <div>Cost Explorer
-                        <span className="small-text">Admin Only</span>
-                    </div>
-                    <div>Performance & Monitoring
-                        <span className="small-text">Testing Results</span>
-                    </div>
-                    <div>System Health
-                        <span className="small-text">No current issues</span>
-                    </div>
-                    <div>LMB Teams
-                        <span className="small-text">Units of each business area</span>
-                    </div>
-                </div>
+        <div className="flex-container">
+          <div>Code Coverage
+            <span className="small-text">98%</span>
+          </div>
+          <div>Security
+            <span className="small-text">No Security Issues</span>
+          </div>
+          <div>Bugs
+            <span className="small-text">7 Critical Items</span>
+          </div>
+          <div>New Relic
+            <span className="small-text">0.114% Error Rate</span>
+          </div>
+          <div>AWS Instances
+            <span className="small-text">67 EC2 Instances</span>
+          </div>
+          <div>Cost Explorer
+            <span className="small-text">Admin Only</span>
+          </div>
+          <div>Performance & Monitoring
+            <span className="small-text">Testing Results</span>
+          </div>
+          <div>System Health
+            <span className="small-text">No current issues</span>
+          </div>
+          <div>LMB Teams
+            <span className="small-text">Units of each business area</span>
+          </div>
+        </div>
 
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
